d6e2a68100117d272d67eda6b32919ec853f2835
augmenta <augmenta@augmenta.lan>
2015-05-22 14:14:11 +0200

    Add a delay between Jack and Borderlands

--- Borderlands/bin/launch_Jack-and-Borderlands.sh
+++ Borderlands/bin/launch_Jack-and-Borderlands.sh
@@ -11,6 +11,6 @@ cd $ORIG_PATH
 cd $SCRIPT_PATH &&
 
 screen -S jackd -d -m env DISPLAY=:0 jackd -P70 -p16 -t2000 -d alsa -dhw:1 -p 128 -n 3 -r 44100 -s
-
+sleep 4
 screen -S borderlands -d -m env DISPLAY=:0 $SCRIPT_PATH/Borderlands
 
